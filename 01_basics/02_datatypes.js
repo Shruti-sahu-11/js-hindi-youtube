f386c1ff000750bb607a646cb02dae985b53112c
Shruti sahu <sahushruti2020@gmail.com>
2025-04-13 20:39:16 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //this will all js code as newer version
+
+//alert(3+3 ) we r using nodejs not browser
+console.log(3+3)
+
+let age = 18
+let isloggedin = false
+let state;
+let a=null
+console.log(age, isloggedin, state)
+//big int is used for large value
+//number 2 to the power 53
+// string = ""
+//boolean = true/false
+//null = standalone value
+//undefined = 
+//symbol=unique
+
+//object
+
+console.log(typeof age) //num
+console.log(typeof state) //undefined
+console.log(typeof a)//object
