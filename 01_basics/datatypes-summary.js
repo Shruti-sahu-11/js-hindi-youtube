c5b5acd3b3d34d09529a5ceca6227aa8ab1803ee
Shruti sahu <sahushruti2020@gmail.com>
2025-04-14 11:29:04 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,29 @@
+// how data s kept in memory and how to access data for this there is two categorization
+//primitve and npon-primitive/
+
+//primitive= whatevre we do changes will reflect in copy onlr
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+//referece type/non-primitive
+//Array, Objects, Functions
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+console.log(id === anotherid);
+
+const bigNumber = 67867879718797n //n reperesnt big number
+
+const heros = ["shaktiman", "naagraj","doga"];
+let myObj={  //object data type
+    name: "hitesh",
+    age: 34,
+}
+
+
+//******////function */
+
+const myFunction = function(){  //function object
+    console.log("hello world")
+}
+
+
