850d86737203f085e1d739f8c393253fd911837f
Shruti sahu <sahushruti2020@gmail.com>
2025-04-15 20:29:33 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,10 @@
+let mydate = new Date()
+console.log(mydate.toString());
+console.log(mydate.toLocaleString());
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleDateString());
+
+console.log(typeof mydate); //object
+
+ 
+
